26893099c8c80b264211702a5b640caba366a397
Aryan-Pourtaheri <3programm.6aagm9@gmail.com>
2025-10-14 15:17:14 +0300

    remove uneccessary functions

--- src/app/components/SessionWrapper.tsx
+++ src/app/components/SessionWrapper.tsx
@@ -4,7 +4,6 @@ import { useSession } from "./SessionContext";
 export default function SessionWrapper({ children }: { children: React.ReactNode }) {
   const session = useSession();
 
-  console.log(session);
 
   return <>{children}</>;
 }
